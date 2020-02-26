921fcdacf04e671e8b43f8ac6d57fa22f092527e
Wesley Jonathan Marcolino <w.jonathan.marcolino@accenture.com>
2020-02-26 14:35:45 -0300

    Fix detekt issues

--- app/src/main/java/br/com/wesjon/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/wesjon/ui/home/HomeFragment.kt
@@ -1,14 +1,11 @@
 package br.com.wesjon.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import br.com.wesjon.R
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
