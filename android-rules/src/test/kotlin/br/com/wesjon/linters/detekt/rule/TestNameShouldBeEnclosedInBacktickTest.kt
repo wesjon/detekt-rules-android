e6491afe897fe8987c7bd1188ef6c53959416a36
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-03-30 21:32:01 -0300

    Remove unused libraries

---
+++ android-rules/src/test/kotlin/br/com/wesjon/linters/detekt/rule/TestNameShouldBeEnclosedInBacktickTest.kt
@@ -0,0 +1,43 @@
+package br.com.wesjon.linters.detekt.rule
+
+import br.com.wesjon.rule.TestNameShouldBeEnclosedInBacktick
+import com.google.common.truth.Truth.assertThat
+import io.gitlab.arturbosch.detekt.test.lint
+import org.junit.Test
+
+class TestNameShouldBeEnclosedInBacktickTest {
+    private val testThatdoesntContainBacktick = """
+        class ExampleUnitTest {
+            @Test
+            fun addition_isCorrect() {
+                assertEquals(4, 2 + 2)
+            }
+        }
+    """.trimIndent()
+
+    private val testThatContainBacktick = """
+        class ExampleUnitTest {
+            @Test
+            fun `addition is correct`() {
+                assertEquals(4, 2 + 2)
+            }
+        }
+    """.trimIndent()
+
+    private val testShouldContainBacktickRule = TestNameShouldBeEnclosedInBacktick()
+
+    @Test
+    fun `test doesnt contains backtick should report issue`() {
+        val findings = testShouldContainBacktickRule.lint(testThatdoesntContainBacktick)
+
+        assertThat(findings).hasSize(1)
+        assertThat(findings[0].message).isEqualTo("O nome do teste precisa conter backtick")
+    }
+
+    @Test
+    fun `test contains backtick find no issues`() {
+        val findings = testShouldContainBacktickRule.lint(testThatContainBacktick)
+
+        assertThat(findings).isEmpty()
+    }
+}
--- sample/src/main/java/br/com/wesjon/MainActivity.kt
+++ sample/src/main/java/br/com/wesjon/MainActivity.kt
@@ -1,18 +1,5 @@
 package br.com.wesjon
 
-import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        navView.setupWithNavController(navController)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
